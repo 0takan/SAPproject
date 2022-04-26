96951ce335d91cda3e72ae8e1b879bb6791bd9aa
0takan <hordienko3000@gmail.com>
2022-04-26 18:37:54 +0300

    Added new controller and model

---
+++ SaleAPproject.Models/Ads.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SaleAPproject.Models
+{
+    public class Ads
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Img { get; set; }
+        public string Desc { get; set; }
+    }
+}
---
+++ SaleAPproject/Controllers/AdsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleAPproject.Controllers
+{
+    public class AdsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
